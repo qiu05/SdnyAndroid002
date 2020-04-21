e48036ba7351d479fe0753de3ee296fb780059ee
qiu05 <1272391177@qq.com>
2020-04-21 11:50:24 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180105/Ch10Activity2.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android02.classroom.sn170507180105;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch10Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
